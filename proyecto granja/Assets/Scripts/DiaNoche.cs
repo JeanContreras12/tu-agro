bcdc71a44eecdf6a5a67c8da0349cb2975eea5c4
JeanContreras12 <j.contrerasmanriquez@uandresbello.edu>
2021-04-28 17:21:38 -0400

    Tutorial listo

--- proyecto granja/Assets/Scripts/DiaNoche.cs
+++ proyecto granja/Assets/Scripts/DiaNoche.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class DiaNoche : MonoBehaviour
 {
     //variables
-    public float min;
+    public float min, grados;
+    public float timeSpeed = 1;
     
     // Update is called once per frame
     void Update()
     {
         //dia =24min
-        min += 1 * Time.deltaTime;// 1 seg = 1 min
+        min += timeSpeed * Time.deltaTime;// 1 seg = 1 min
         //60min=1hr   24hr
         if (min >= 1440)//1140 =24
         {
             min = 0;
         }
+        grados = min / 4;
+        this.transform.localEulerAngles = new Vector3 (grados, -90f, 0f);
 
         
     }
